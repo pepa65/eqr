89498f2e27ef11c96772e9290c98742dfd30216a
gitlab.com/pepa65 <pepa65@passchier.net>
2024-10-05 18:45:10 +0700

    Binary name on commandline

--- build.rs
+++ build.rs
@@ -23,7 +23,9 @@ fn main() {
     // Generate & write man page.
     let mut buffer: Vec<u8> = Vec::new();
     Man::new(cmd.clone()).render(&mut buffer).expect("unable to generate man page");
-    fs::write(man_dir.join("qr.1"), buffer).expect("unable to write man page");
+    let buf = std::str::from_utf8(&buffer).unwrap();
+    let new = buf.replace("fBeqr", "fBqr");
+    fs::write(man_dir.join("qr.1"), new).expect("unable to write man page");
 
     // Generate shell completions.
     for shell in [Bash, Elvish, Fish, PowerShell, Zsh] {
